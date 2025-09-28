0963a5bd40ab25c22bf2f175b4f1e0bfc46a4b16
Martin Vlacil <martinvlacil.mv@gmail.com>
2025-09-28 15:50:30 +0200

    hi

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
 
 
 def attack_exercise_3_2():
-    initial_message = "I am a message, surely no cryptographers will make me suffer by encrypting and decrypting me for testing purpo-UAAAAAAAAAAAAAAG"
+    initial_message = "I am a message, surely no cryptographers will make me suffer by encrypting and decrypting me for testing purpo-UAAAAAAAAAAAAAAGH"
     key = aes_lib.keygen()
     print(f"Initial message: {initial_message}")
     ciphertext = aes_lib.encrypt(initial_message, key)
@@ -22,7 +22,7 @@ def attack_exercise_3_2():
 
 def successful_test_exercise_3_1():
     # There is no error handling for a message that is not a multple of 128-bit, when changing, be careful when changing
-    initial_message = "I am a message, surely no cryptographers will make me suffer by encrypting and decrypting me for testing purpo-UAAAAAAAAAAAAAAG"
+    initial_message = "I am a message, surely no cryptographers will make me suffer by encrypting and decrypting me for testing purpo-UAAAAAAAAAAAAAAGH"
     key = aes_lib.keygen()
     print(f"Initial message: {initial_message}")
     ciphertext = aes_lib.encrypt(initial_message, key)
